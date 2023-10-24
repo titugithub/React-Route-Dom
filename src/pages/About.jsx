38ee1ee77134a05edf1e09dc912d91f5c5b733a2
Titu <titufrk@gmail.com>
2023-10-24 23:04:38 +0600

    cc

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./About.css"
+import Header from "./Header"
+
+function About() {
+  return (
+   
+    <div className='About'>
+    <Header/>
+      <h1>This is About Page</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Home.css"
+import Header from "./Header"
+
+
+
+
+function Home() {
+  return (
+    <div className='Home'>
+         <Header/>
+      <h1>This is Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
